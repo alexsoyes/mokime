9045286461b8f0cd8baed3064e93dd249732d8d4
= <=>
2020-02-16 14:49:55 +0100

    :art: - Remove Bulma's card & hero style, customize new cards

--- template-parts/entry/entry-article.php
+++ template-parts/entry/entry-article.php
@@ -2,28 +2,29 @@
          itemtype="http://schema.org/BlogPosting">
     <div class="card has-margin-top-3">
 		<?php $post_image = mokime_get_post_thumbnail_url( get_post() ); ?>
-        <div class="card-image"<?php if ( $post_image ): ?> style="<?php echo sprintf( " background-image: url('%s')", $post_image ) ?>"<?php endif ?>>
+        <div class="card-image"<?php if ( $post_image ): ?> style="<?php echo sprintf( " background-image: url('%s')", $post_image ) ?>"<?php endif ?>></div>
+        <!-- .card--image -->
+
+        <div class="card-content">
+
+			<?php get_template_part( 'template-parts/entry/entry-article-categories' ); ?>
+
+            <h3 class="card-title has-text-weight-bold has-margin-bottom-075" itemprop="headline">
+                <a href="<?php the_permalink() ?>" class="color-secondary">
+					<?php the_title(); ?>
+                </a>
+            </h3><!-- .title -->
+
+            <p class="subtitle is-6 has-text-justified has-text-overflowed has-margin-bottom-1"><?php echo get_the_excerpt(); ?></p>
+
             <div class="card-date">
                 <time class="is-small-text" datetime="<?php echo get_the_date( 'c' ); ?>"
                       itemprop="datePublished">
                     <small><?php echo get_the_date( 'j F Y' ); ?></small>
                 </time>
             </div><!-- .card-date -->
-        </div><!-- .card--image -->
 
-	    <div class="card-content">
-		    <h3 class="card-title has-text-weight-bold has-margin-top-1 has-margin-bottom-075" itemprop="headline">
-			    <a href="<?php the_permalink() ?>" class="color-secondary">
-					<?php the_title(); ?>
-                </a>
-            </h3><!-- .title -->
-
-			<?php get_template_part( 'template-parts/entry/entry-article-categories' ); ?>
-
-            <p class="subtitle is-6 has-text-justified has-text-overflowed has-margin-bottom-1"><?php echo get_the_excerpt(); ?></p>
-
-
-	    </div><!-- .card-content -->
+        </div><!-- .card-content -->
 
     </div><!-- .card -->
 
---
+++ template-parts/entry/entry-breadcrumb.php
@@ -0,0 +1,7 @@
+<div class="wp-block-columns breadcrumb">
+	<?php if ( function_exists( 'yoast_breadcrumb' ) ): ?>
+        <div class="wp-block-column wp-block-column-100 entry-content">
+			<?php yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' ); ?>
+        </div>
+	<?php endif; ?>
+</div><!-- .breadcrumb -->
