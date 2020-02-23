97c7860e200c2e7f7fccd8807f3e156c5d2a6b6e
= <=>
2020-02-23 09:47:20 +0100

    :wrench: New release script

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 set -e
 
-if [ "$1" == "" ]; then
-  echo 'You must specify a version number.'
-  exit 0;
-fi
-
-OUTPUT_FILE="versions/mokime.$1.zip"
+OUTPUT_FILE="versions/mokime.zip"
 
 if [ -e "${OUTPUT_FILE}" ]; then
   rm -v "${OUTPUT_FILE}"
 fi
 
-cd ..
 zip -9 -rqq "${OUTPUT_FILE}" . \
     -x="*docs/*" \
     -x="*README.md*" \
     -x="*.json*" \
     -x="*.git/*" \
+    -x="*versions*" \
     -x="*.gitignore*" \
     -x="*node_modules/*" \
     -x="*scripts/*"
