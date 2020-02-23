da540eb733f26fae47142c9f60dcb8ecbb8d3a7a
= <=>
2020-02-23 14:19:38 +0100

    :bug: Undo PHPFixer line truncate...

--- functions.php
+++ functions.php
@@ -80,6 +80,31 @@ function mokime_theme_support() {
 		)
 	);
 
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+		)
+	);
+
 	// Custom background color.
 	add_theme_support(
 		'custom-background',
