56fba0975551d5df4279db60a8560c0b37b93d25
= <=>
2020-02-29 08:16:23 +0100

    :wrench: Disable source map in style.css

--- scripts/make-sass.sh
+++ scripts/make-sass.sh
@@ -2,6 +2,6 @@
 
 ./node_modules/node-sass/bin/node-sass ./assets/css/main.scss main.css
 
-./node_modules/postcss-cli/bin/postcss main.css --use autoprefixer -o style.css
+./node_modules/postcss-cli/bin/postcss main.css --use autoprefixer --output style.css --no-map
 
 rm -v main.css
