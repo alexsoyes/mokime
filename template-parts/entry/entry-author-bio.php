d1ee8fca1b48a81f84a0da2cda4a91b5e082f18d
Alex Soyer <alexandre.soyer@codein.fr>
2020-03-17 17:24:36 +0100

    :bug: Do not display author if unchecked in customizer but still use HTML markups for Schema

--- template-parts/entry/entry-author-bio.php
+++ template-parts/entry/entry-author-bio.php
@@ -1,51 +1,52 @@
-<?php
-
-if ( (bool) get_theme_mod( 'single_post_author_bio', true ) ) : ?>
-
-	<div itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-		<div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-			<meta itemprop="url" content="<?php echo esc_html( get_custom_logo_url() ); ?>">
-		</div>
-		<meta itemprop="name" content="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>">
+<div itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+	<?php if ( has_custom_logo() ) : ?>
+	<div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+		<meta itemprop="url" content="<?php echo esc_html( get_custom_logo_url() ); ?>">
 	</div>
+	<meta itemprop="name" content="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>">
+	<?php endif; ?>
+</div>
 
-	<div itemprop="author" itemscope itemtype="https://schema.org/Person" class="author-box has-text-align-center">
-
-		<figure class="author-image">
-			<?php
-			echo get_avatar(
-				get_the_author_meta( 'ID' ),
-				96,
-				'',
-				get_the_author(),
-				array(
-					'itemprop' => 'image',
-				)
-			);
+<div itemprop="author" itemscope itemtype="https://schema.org/Person"
+	 class="author-box has-text-align-center
+	 <?php
+		if ( ! (bool) get_theme_mod( 'single_post_author_bio', true ) ) :
 			?>
-		</figure><!-- .author-image -->
+			 display-none<?php endif; ?>">
 
-		<div class="media-content-center content">
+	<figure class="author-image">
+		<?php
+		echo get_avatar(
+			get_the_author_meta( 'ID' ),
+			96,
+			'',
+			get_the_author(),
+			array(
+				'itemprop' => 'image',
+			)
+		);
+		?>
+	</figure><!-- .author-image -->
 
-			<p itemprop="name" class="h3 has-text-weight-bold"><?php echo esc_html( get_the_author() ); ?></p>
+	<div class="media-content-center content">
 
-			<div class="author-description">
+		<p itemprop="name" class="h3 has-text-weight-bold"><?php echo esc_html( get_the_author() ); ?></p>
+
+		<div class="author-description">
 				<span itemprop="description">
 					<?php echo wp_kses_post( wpautop( get_the_author_meta( 'description' ) ) ); ?>
 				</span>
-				<a class="author-link"
-				   itemprop="url"
-				   href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
-				   rel="author">
-					<?php
-					// phpcs:ignore WordPress.Security.EscapeOutput.UnsafePrintingFunction
-					_e( 'View Archive <span aria-hidden="true">&rarr;</span>', 'mokime' );
-					?>
-				</a>
-			</div><!-- .author-description -->
+			<a class="author-link"
+			   itemprop="url"
+			   href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"
+			   rel="author">
+				<?php
+                // phpcs:ignore WordPress.Security.EscapeOutput.UnsafePrintingFunction
+				_e( 'View Archive <span aria-hidden="true">&rarr;</span>', 'mokime' );
+				?>
+			</a>
+		</div><!-- .author-description -->
 
-		</div><!-- .media-content-center -->
+	</div><!-- .media-content-center -->
 
-	</div><!-- .author-box -->
-
-<?php endif; ?>
+</div><!-- .author-box -->
