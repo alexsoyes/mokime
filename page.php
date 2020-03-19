4caf84845ecca2eefcd49d4d3b6f69e113085cd1
Dinath <dinath.contact@gmail.com>
2020-03-19 14:04:53 +0100

    :bug: Remove extra closing div

--- page.php
+++ page.php
@@ -1,4 +1,11 @@
-<?php get_header();
+<?php
+/**
+ * Display content type "page".
+ *
+ * @package mokime
+ */
+
+get_header();
 
 global $header_class, $header_image, $header_title, $header_description, $has_background_image;
 
@@ -6,18 +13,19 @@ $header_class = '';
 $header_title = get_the_title();
 $header_image = mokime_get_post_thumbnail_url( get_post() );
 
-		the_post();
-		get_template_part( 'template-parts/content/content-page' ); ?>
-	</div><!-- .entry-content -->
+the_post();
+get_template_part( 'template-parts/content/content-page' );
 
-<?php
 
-if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
-	wpcf7_enqueue_scripts();
-}
+if ( (bool) get_theme_mod( 'performance_enable_only_page_contact_form_7', false ) ) {
 
-if ( function_exists( 'wpcf7_enqueue_styles' ) ) {
-	wpcf7_enqueue_styles();
+	if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
+		wpcf7_enqueue_scripts();
+	}
+
+	if ( function_exists( 'wpcf7_enqueue_styles' ) ) {
+		wpcf7_enqueue_styles();
+	}
 }
 
 get_footer();
