cbc1b4b2471e2f6fefccaef2a5aadf9ae57ced1c
Dinath <dinath.contact@gmail.com>
2020-03-21 11:27:05 +0100

    :art: Change template-full-width.php structure

--- templates/template-full-width.php
+++ templates/template-full-width.php
@@ -14,32 +14,35 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="profile" href="https://gmpg.org/xfn/11"/>
-	<?php wp_head(); ?>
+	<?php
+	wp_head();
+	$body_class = array();
+	array_push( $body_class, 'template-full-width' );
+
+	if ( has_post_thumbnail() ) {
+		array_push( $body_class, 'overlay' );
+		?>
+		<style>
+			html {
+				background-image: url("<?php echo esc_url( get_the_post_thumbnail_url() ); ?>");
+			}
+		</style>
+		<?php
+	}
+	?>
 </head>
 
+<body <?php body_class( $body_class ); ?>>
+<?php wp_body_open(); ?>
+
+<main>
+	<div class="entry-content">
+		<h1 class="has-text-align-center"><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+	</div>
+</main>
+
 <?php
-$body_class = array();
-array_push( $body_class, 'template-full-width' );
-
-if ( has_post_thumbnail() ) {
-	array_push( $body_class, 'overlay' );
-}
-?>
-
-<body <?php body_class( 'template-full-width' ); ?>>
-<?php
-
-if ( has_post_thumbnail() ) {
-	?>
-	<style>
-		body {
-			background-image: url("<?php echo esc_url( get_the_post_thumbnail_url() ); ?>");
-		}
-	</style>
-	<?php
-}
-wp_body_open();
-the_content();
 mokime_template_style();
 wp_footer();
 ?>
