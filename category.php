f18668c1976dae8c288125179b829ce95ac83e8f
Dinath <dinath.contact@gmail.com>
2020-04-04 15:42:41 +0200

    :mag: Display pagination index on paginated pages

--- category.php
+++ category.php
@@ -10,6 +10,13 @@ $header_title = get_the_archive_title();
 if ( ! is_paged() ) {
 	$header_description  = get_the_archive_description();
 	$header_description .= mokime_get_the_child_categories();
+} else {
+	global $wp_query;
+	$header_description = sprintf(
+		'<p class="h4 tag tag-primary">Page %d / %d</p>',
+		$paged,
+		$wp_query->max_num_pages
+	);
 }
 
 get_template_part( 'template-parts/content/content-archive' );
