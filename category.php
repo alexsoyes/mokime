936260b360d8784dd9158dd700decceb87c8c3cd
Dinath <dinath.contact@gmail.com>
2020-04-04 15:17:31 +0200

    :mag: Do not display categories content on paginated pages

--- category.php
+++ category.php
@@ -4,10 +4,13 @@ get_header();
 
 global $header_class, $header_image, $header_title, $header_description, $has_background_image;
 
-$header_image        = get_theme_mod( 'header_image' );
-$header_title        = get_the_archive_title();
-$header_description  = get_the_archive_description();
-$header_description .= mokime_get_the_child_categories();
+$header_image = get_theme_mod( 'header_image' );
+$header_title = get_the_archive_title();
+
+if ( ! is_paged() ) {
+	$header_description  = get_the_archive_description();
+	$header_description .= mokime_get_the_child_categories();
+}
 
 get_template_part( 'template-parts/content/content-archive' );
 
