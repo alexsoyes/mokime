85c72b85dfd856aea3d9c78cead2da74489f8ad7
Dinath <dinath.contact@gmail.com>
2020-04-11 12:59:53 +0200

    :bu:g Display front-page title depending on is_home() or not

--- front-page.php
+++ front-page.php
@@ -6,13 +6,9 @@ global $header_class, $header_image, $header_title, $header_description, $has_ba
 
 $header_class         = ' has-text-align-center';
 $header_image         = get_header_image();
-$header_title         = get_the_title();
+$header_title         = ( ! is_home() && is_front_page() ) ? get_the_title() : get_bloginfo( 'name' );
 $header_description   = get_bloginfo( 'description' );
 $has_background_image = isset( $header_image ) && ( $header_image && 'remove-header' !== $header_image );
-
-if ( ! $header_title ) {
-	$header_title = get_bloginfo( 'name' );
-}
 
 get_template_part( 'template-parts/content/content-home' );
 
