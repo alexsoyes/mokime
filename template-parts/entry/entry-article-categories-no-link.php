d06786447affc6e75fa675b979cca19f9f1d901d
Dinath <dinath.contact@gmail.com>
2020-04-12 12:58:16 +0200

    :bug: No container for list items

--- template-parts/entry/entry-article-categories-no-link.php
+++ template-parts/entry/entry-article-categories-no-link.php
@@ -7,6 +7,14 @@
 
 $categories = get_the_category();
 
+if ( ! empty( $categories ) ) {
+	echo '<ul class="tags">';
+}
+
 foreach ( $categories as $category ) {
 	echo wp_kses_post( sprintf( '<li class="tag">%s</li>', $category->name ) );
+}
+
+if ( ! empty( $categories ) ) {
+	echo '</ul>';
 }
